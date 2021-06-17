da3a441c487e255baf4b0169c370081e2aa1e756
smmartprogrammer <partabrajp@gmail.com>
2021-06-18 03:03:19 +0500

    Remove one line from the project, just for checking the git commits

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,6 @@ const Header = () => {
                 <Navbar.Brand href="#home">Shoe Store</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    {/* <Nav className="mr-auto"> */}
                     <Nav >
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/products">Product</Nav.Link>
